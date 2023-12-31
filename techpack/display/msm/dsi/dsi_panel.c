41040b78d5f1915f99ba23e5005992c69d1b7717
tumbalrais <155171364+tumbalrais@users.noreply.github.com>
2023-12-31 10:01:03 +0000

    hook powersuspend to dsi_panel.c

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -492,6 +492,11 @@ EXPORT_SYMBOL(set_fts_ts_variant);
 static int dsi_panel_power_on(struct dsi_panel *panel)
 {
 	int rc = 0;
+
+	#ifdef CONFIG_POWERSUSPEND
+ 		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+	#endif
+
 #ifdef CONFIG_TARGET_PROJECT_K7T
 	int power_status = DRM_PANEL_BLANK_UNBLANK;
 	struct drm_panel_notifier notifier_data;
@@ -561,6 +566,10 @@ EXPORT_SYMBOL(set_lcd_reset_gpio_keep_high);
 static int dsi_panel_power_off(struct dsi_panel *panel)
 {
 	int rc = 0;
+
+	#ifdef CONFIG_POWERSUSPEND
+ 		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+	#endif
 
 #ifdef CONFIG_TARGET_PROJECT_C3Q
 	//usleep_range(11000, 11010);	
